0e826e3f402e72260c1b0baef180e2ad8d8ee138
Michael Maggs <michaelmaggs@gmail.com>
2026-06-02 22:30:05 -0700

    initial commit

---
+++ tf-files/variables.tf
@@ -0,0 +1,15 @@
+variable "git-name" {
+  default = "maggs"
+}
+
+variable "git-token" {
+  default = "XXXXXXXXXXXXX"
+}
+
+variable "key-name" {
+  default = "core"
+}
+
+variable "hosted-zone" {
+  default = "infinitelyloopy.com"
+}
